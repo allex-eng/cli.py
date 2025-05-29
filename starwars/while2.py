e531b0e832511e0fe9a194098d6186ea3d5c885a
allex-eng <allexvitorgomes@gmail.com>
2025-05-29 20:40:51 -0300

    while

---
+++ starwars/while2.py
@@ -0,0 +1,25 @@
+import sys
+
+numero = None
+
+try:
+    numero = int(input('Digite um numero para calcular se é primo: '))
+
+except ValueError: 
+    sys.exit('ERRO: Digite um número inteiro positivo.')
+
+except Exception as exc:
+    sys.exit(f'ERRO: {exc}')
+
+else:
+    divisores = 1
+    contDivisor = 0 # poderia tambem ja iniciar com 2
+    while divisores <= numero:
+        if (numero % divisores) == 0:
+            contDivisor += 1
+        divisores += 1
+
+    if contDivisor == 2:
+        print(f'{numero} é primo.')
+    else:
+        print(f'{numero} não é primo')
---
+++ starwars/while3.py
@@ -0,0 +1,30 @@
+
+
+import sys
+
+numero = None
+
+try:
+    numero = int(input('Digite um numero para calcular seu fatorial: '))
+
+except ValueError: 
+    sys.exit('ERRO: Digite um número inteiro positivo')
+
+except Exception as exc:
+    sys.exit(f'ERRO: {exc}')
+
+else:
+    if numero < 0:
+        print('Não existe fatorial.')
+    elif numero < 2:
+        print(f'Fatorial de {numero} = 1')
+
+    else:
+        resultado = numero
+        contador = numero
+        
+        while contador > 1:
+            resultado *= (numero - 1)
+            contador -= 1
+        
+        print(f'Fatorial de {numero} = {resultado}')
