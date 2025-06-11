a837781952ca2d3fcb5d3d341deaf94e21bad6d1
allex-eng <allexvitorgomes@gmail.com>
2025-06-11 09:08:17 -0300

    stings

---
+++ starwars/sting.py
@@ -0,0 +1,6 @@
+strtexto = input('Digite: ')
+
+posicao = len(strtexto)
+while posicao > 0:
+    print(strtexto[0:posicao])
+    posicao -= 1
---
+++ starwars/sting2.py
@@ -0,0 +1,6 @@
+strtexto = input('Digite: ')
+
+posicao = 1
+while posicao <= len(strtexto):
+    print(strtexto[0:posicao])
+    posicao += 1
